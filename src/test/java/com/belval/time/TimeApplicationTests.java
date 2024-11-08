763ffa65a5fc9834932f58065bfd85d343f6d82b
Wendel de Sousa Gonçalves <wendelsg2016@gmail.com>
2024-11-08 20:24:36 -0300

    Versão inicial

---
+++ src/test/java/com/belval/time/TimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belval.time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
